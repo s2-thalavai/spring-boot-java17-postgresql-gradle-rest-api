f5c50e836221d6bc8397349a26d8c2657142596d
SivasankarThalavai <s2.thalavai@gmail.com>
2024-11-14 17:48:01 +0530

    initial commit

---
+++ src/main/java/com/sivasankar/spring_boot_java17_postgresql_gradle_rest_api/SpringBootJava17PostgresqlGradleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sivasankar.spring_boot_java17_postgresql_gradle_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava17PostgresqlGradleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava17PostgresqlGradleRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivasankar/spring_boot_java17_postgresql_gradle_rest_api/SpringBootJava17PostgresqlGradleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivasankar.spring_boot_java17_postgresql_gradle_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJava17PostgresqlGradleRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
